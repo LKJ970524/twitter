f09c115b23845110e4ef3c79194a0fbc89e50294
LKJ970524 <dlrbwjd7096@naver.com>
2024-04-21 23:08:44 +0900

    feature/3-3 AuthContextf로 전역상태관리

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,11 +1,13 @@
 import { BsHouse } from "react-icons/bs";
 import { BiUserCircle } from "react-icons/bi";
-import { MdLogout } from "react-icons/md"
+import { MdLogout, MdLogin } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
-
+import { useContext } from "react";
+import AuthContext from "context/AuthContext";
 
 export default function MenuList() {
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
   return (
     <div className="footer">
@@ -18,10 +20,17 @@ export default function MenuList() {
           <BiUserCircle />
           Profile
         </button>
-        <button type="button" onClick={() => navigate("/")}>
-          <MdLogout />
-          Logout
-        </button>
+        {user === null ? (
+          <button type="button" onClick={() => navigate("/users/login")}>
+            <MdLogin />
+            Login
+          </button>
+        ) : (
+          <button type="button" onClick={() => navigate("/")}>
+            <MdLogout />
+            Logout
+          </button>
+        )}
       </div>
     </div>
   );
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,34 @@
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { app } from "firebaseApp";
+
+interface AuthProps {
+  children: ReactNode;
+}
+
+const AuthContext = createContext({
+  user: null as User | null,
+});
+
+export const AuthContextProvider = ({ children }: AuthProps) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+  const auth = getAuth(app);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setCurrentUser(user);
+      } else {
+        setCurrentUser(null);
+      }
+    });
+  }, [auth]);
+
+  return (
+  <AuthContext.Provider value={{user: currentUser}}>
+    {children}
+  </AuthContext.Provider>
+  )
+};
+
+export default AuthContext
