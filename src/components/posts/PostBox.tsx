985bd20ad38fbf5b1d7e7e0480ef54fe01f21f77
LKJ970524 <dlrbwjd7096@naver.com>
2024-05-05 15:35:04 +0900

    feature/3-9 Firestore로 Follow 기능 구현

--- src/components/posts/PostBox.tsx
+++ src/components/posts/PostBox.tsx
@@ -1,5 +1,12 @@
+import FollowingBox from "components/following/FollowingBox";
 import AuthContext from "context/AuthContext";
-import { arrayRemove, arrayUnion, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import {
+  arrayRemove,
+  arrayUnion,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 import { ref, deleteObject } from "firebase/storage";
 import { db, storage } from "firebaseApp";
 import { PostProps } from "pages/home";
@@ -15,64 +22,74 @@ interface PostBoxProps {
 
 export default function PostBox({ post }: PostBoxProps) {
   const { user } = useContext(AuthContext);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const imageRef = ref(storage, post?.imageUrl);
 
   const toggleLike = async () => {
-    const postRef = doc(db, 'posts', post.id)
+    const postRef = doc(db, "posts", post.id);
 
     if (user?.uid && post?.likes?.includes(user?.uid)) {
-      await updateDoc (postRef, {
+      await updateDoc(postRef, {
         likes: arrayRemove(user?.uid),
-        likeCount: post?.likeCount ? post?.likeCount -1 : 0,
+        likeCount: post?.likeCount ? post?.likeCount - 1 : 0,
       });
     } else {
-      await updateDoc (postRef, {
+      await updateDoc(postRef, {
         likes: arrayUnion(user?.uid),
-        likeCount: post?.likeCount ? post?.likeCount +1 : 1,
-      })
+        likeCount: post?.likeCount ? post?.likeCount + 1 : 1,
+      });
     }
-  }
+  };
 
-  const handleDelete = async() => {
-    const confirm = window.confirm('해당 게시글을 삭제하시겠습니까?')
-    if(confirm) {
+  const handleDelete = async () => {
+    const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
+    if (confirm) {
       // storage image 먼저 삭제
 
-
-      if(post?.imageUrl) {
+      if (post?.imageUrl) {
         deleteObject(imageRef).catch((error) => {
           console.log(error);
-        })
+        });
       }
 
-      await deleteDoc(doc(db, 'posts', post.id))
-      toast.success('게시글을 삭제했습니다.')
-      navigate('/')
+      await deleteDoc(doc(db, "posts", post.id));
+      toast.success("게시글을 삭제했습니다.");
+      navigate("/");
     }
   };
 
   return (
     <div className="post__box" key={post?.id}>
-      <Link to={`/posts/${post?.id}`}>
-        <div className="post__box-profile">
-          <div className="post__flex">
-            {post?.profileUrl ? (
-              <img
-                src={post?.profileUrl}
-                alt="profile"
-                className="post__box-profile-img"
-              />
-            ) : (
-              <FaUserCircle className="post__box-profile-icon" />
-            )}
-            <div className="post__email">{post?.email}</div>
-            <div className="post__createdAt">{post?.createdAt}</div>
+      <div className="post__box-profile">
+        <div className="post__flex">
+          {post?.profileUrl ? (
+            <img
+              src={post?.profileUrl}
+              alt="profile"
+              className="post__box-profile-img"
+            />
+          ) : (
+            <FaUserCircle className="post__box-profile-icon" />
+          )}
+          <div className="post__flex--between">
+            <div className="post__flex">
+              <div className="post__email">{post?.email}</div>
+              <div className="post__createdAt">{post?.createdAt}</div>
+            </div>
+            <FollowingBox post={post} />
           </div>
+        </div>
+        <Link to={`/posts/${post?.id}`}>
           <div className="post__box-content">{post?.content}</div>
           {post?.imageUrl && (
             <div className="post__image-div">
-              <img src={post?.imageUrl} alt="post_img" className="post__image" width={150} height={150} />
+              <img
+                src={post?.imageUrl}
+                alt="post img"
+                className="post__image"
+                width={100}
+                height={100}
+              />
             </div>
           )}
           <div className="post-form__hashtags-outputs">
@@ -82,8 +99,8 @@ export default function PostBox({ post }: PostBoxProps) {
               </span>
             ))}
           </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
       <div className="post__box-footer">
         {user?.uid === post?.uid && (
           <>
